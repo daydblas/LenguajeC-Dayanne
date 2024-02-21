537ba95f365ec19ce8cbde75d0f77c822fc59833
daydblas <160264199+daydblas@users.noreply.github.com>
2024-02-21 08:17:24 -0600

    Suma de numeros del 1 al 100 con while

---
+++ suma y numeros C.c
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+
+ main()
+ {
+
+        int i = 1 ;
+        int suma=0;
+
+        while(i<=100)
+        {
+
+          suma=suma+i;
+
+            i++;
+        }
+
+         printf("%d\n",suma);
+}
